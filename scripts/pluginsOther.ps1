19476d0b1f55e2c090370e4ddb206277156f6a8e
npx18k <157927691+npx18k@users.noreply.github.com>
2025-01-09 22:40:46 +0200

    Update pluginsOther.ps1

--- scripts/pluginsOther.ps1
+++ scripts/pluginsOther.ps1
@@ -2,7 +2,7 @@ $PluginsDirectory = "$env:USERPROFILE\Desktop\server\plugins"
 
 $TerraPlusMinusUrl = "https://github.com/BTE-Germany/TerraPlusMinus/releases/download/v1.4.0/terraplusminus-1.4.0.jar"
 $LoginSecurityUrl = "https://ci.codemc.io/view/Author/job/lenis0012/job/LoginSecurity/lastSuccessfulBuild/artifact/target/LoginSecurity-Spigot-3.3.1-SNAPSHOT.jar"
-$BuildersUtilitiesUrl "https://ci.athion.net/job/Builders-Utilities/lastSuccessfulBuild/artifact/build/libs/Builders-Utilities-2.1.1-124.jar"
+$BuildersUtilitiesUrl = "https://ci.athion.net/job/Builders-Utilities/lastSuccessfulBuild/artifact/build/libs/Builders-Utilities-2.1.1-124.jar"
 
 try {
     Invoke-WebRequest -Uri $TerraPlusMinusUrl -OutFile "$PluginsDirectory\terraplusminus-1.4.0.jar"
